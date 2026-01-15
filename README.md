aa6a2dc051d4cadab6c26bab93545c4d0df22930
Michael L. Collard <collard@uakron.edu>
2026-01-15 08:26:43 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Rainfall
+
+Report of average and maximum rainfall
+
+* rainfall.cpp - Main program
+* demo.csv - Rainfall data
+* demo.txt - Output of rainfall program with demo.csv data
+* CMakeLists.txt - build-generator file
+* README.md
+
+Create build file:
+
+```console
+mkdir build
+cd build
+cmake ..
+```
+
+Build:
+
+```console
+make
+```
+
+Run with demo.csv data:
+
+```console
+make run
+```
+
+Check design changes:
+
+```console
+make check
+```
---
+++ rainfall.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<float> rainfall;
+    float t, m, n;
+    while (cin >> n) {
+        rainfall.push_back(n);
+    }
+    if (!rainfall.size()) {
+        cout << "Error: no rainfall data";
+        return 1;
+    }
+    t = rainfall[0];
+    m = rainfall[0];
+    for (vector<float>::size_type i = 1; i < rainfall.size(); ++i) {
+        t += rainfall[i];
+        if (rainfall[i] > m)
+            m = rainfall[i];
+    }
+    cout << "| Hourly Rainfall | Inches in 100s |" << '\n';
+    cout << "|:----------------|----------------|" << '\n';
+    cout << "| Average         |     " << left << setw(10) << fixed << setprecision(2) << (t / rainfall.size()) << " |" << '\n';
+    cout << "| Heaviest        |     " << left << setw(10) << m << " |" << '\n';
+}
